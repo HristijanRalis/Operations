06a2e1d79bbc1ef230af72c9beab9e920a14ad11
Hristijan Ralevski <hristijan.ralevski@hotmail.com>
2025-04-08 11:57:13 +0200

    Add arguments in function

--- starter-files/script.js
+++ starter-files/script.js
@@ -1,2 +1,17 @@
 // SCRIPT
 // Logical Operators
+
+function drivingLicence(firstName, years, hasLicence) {
+  if (years >= 18 && hasLicence) {
+    return `${firstName} have ${years} and can drive a car! `;
+  } else if (years >= 18 && !hasLicence) {
+    return `${firstName} have ${years} cannot drive a car because don't have licence yet `;
+  } else {
+    return `${firstName} have ${years} and cannot drive a car!  `;
+  }
+}
+
+console.log(drivingLicence("Nikola", 18, true));
+console.log(drivingLicence("Hristijan", 18, true));
+console.log(drivingLicence("Nikola", 16, true));
+
