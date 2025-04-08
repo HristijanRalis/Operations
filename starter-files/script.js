10c59082d9233de86167249c8fbe3dc10a15fd9f
Hristijan Ralevski <hristijan.ralevski@hotmail.com>
2025-04-08 10:15:36 +0200

    Done with sum,sub,mul,div

--- starter-files/script.js
+++ starter-files/script.js
@@ -1,1 +1,19 @@
 // SCRIPT
+
+// + , - , / , * , /
+const a = 9,
+  b = 6;
+
+// +
+let sum = a + b;
+// -
+let sub = a - b;
+// '*'
+let mul = a * b;
+// '/'
+let div = a / b;
+
+console.log("Sum: ", sum);
+console.log("Subtraction: ", sub);
+console.log("Multiply: ", mul);
+console.log("Division", div);
