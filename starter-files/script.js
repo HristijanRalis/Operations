1c86ed8f3a03bf641bcc0255b8fd0184445868c8
Hristijan Ralevski <hristijan.ralevski@hotmail.com>
2025-04-08 10:25:41 +0200

    Done with operation with condition

--- starter-files/script.js
+++ starter-files/script.js
@@ -1,5 +1,6 @@
 // SCRIPT
 
+// ====================================
 // + , - , / , * , /
 const a = 9,
   b = 6;
@@ -17,3 +18,18 @@ console.log("Sum: ", sum);
 console.log("Subtraction: ", sub);
 console.log("Multiply: ", mul);
 console.log("Division", div);
+// ======================================
+
+// Operators with conditions
+
+function calculations(a, b, operator) {
+  if (operator === "+") return a + b;
+  if (operator === "-") return a - b;
+  if (operator === "*") return a * b;
+  if (operator === "/") return a / b;
+}
+
+console.log("Sum:", calculations(2, 4, "+"));
+console.log("Subtraction:", calculations(7, 4, "-"));
+console.log("Multiply:", calculations(2, 4, "*"));
+console.log("Division", calculations(2, 4, "/"));
