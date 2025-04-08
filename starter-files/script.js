8adc634b56e4ac573497d916fe2c8f3c5bbd2a7c
Hristijan Ralevski <hristijan.ralevski@hotmail.com>
2025-04-08 12:51:23 +0200

    Added several arrays with for loop

--- starter-files/script.js
+++ starter-files/script.js
@@ -1,3 +1,35 @@
 // SCRIPT
 // =====================
 // ARRAYS
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(arr);
+
+// For Loop
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
+// ====================================
+console.log("=========================");
+for (let i = 0; i < arr.length; i++) {
+  if (i % 2 === 0) {
+    console.log(`The number ${i} is even!`);
+  } else {
+    console.log(`The number ${i} is odd!`);
+  }
+}
+
+// =====================================
+console.log("=========================");
+const numbers = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
+  23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41,
+  42, 43, 44, 45, 46, 47, 48, 49, 50,
+];
+
+// FOR Loop
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] % 2 === 0) {
+    numbers[i] = "foo";
+  }
+}
+console.log(numbers);
